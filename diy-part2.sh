9d45ac5811081133fa864099103c3336bd1de4b4
QWQ2025 <90580525+QWQ2025QWQ@users.noreply.github.com>
2026-07-27 18:07:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,14 +19,6 @@ sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generat
 echo 'CONFIG_CCACHE=y' >> .config
 
 # Modify default theme
-sed -i 's/+luci-theme-bootstrap/+luci-theme-alpha/g' feeds/luci/collections/luci-light/Makefile
-mkdir -p files/etc/uci-defaults
-cat > files/etc/uci-defaults/99-luci-theme-alpha << 'EOF'
-#!/bin/sh
-uci set luci.main.mediaurlbase=/luci-static/alpha
-uci commit luci
-exit 0
-EOF
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
